aba859397fe98a12af23274943257cd535f57bba
Evan Solomon <evanlsolomon@gmail.com>
2018-07-21 18:02:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function prependDriver(name){
 }
 
 function removeLastDriver(){
-  const removeLastDriver = slice.drivers(0, drivers.length-1);
+  const removeLastDriver = drivers.slice(0, drivers.length-1);
   return removeLastDriver;
 }
