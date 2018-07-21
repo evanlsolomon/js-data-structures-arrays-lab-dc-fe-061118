3768179af191b77681d8c93ae7cd8b9bbba0a92f
Evan Solomon <evanlsolomon@gmail.com>
2018-07-21 17:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver(name){
 }
 
 function appendDriver(name){
-const appendDriver = [...drivers, 'name'];
+const appendDriver = [...drivers, name];
 return appendDriver;
 }
