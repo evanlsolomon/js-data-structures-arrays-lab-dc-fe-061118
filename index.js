dc788dbc8ca2d01368ee41f7999b7331f9a601e0
Evan Solomon <evanlsolomon@gmail.com>
2018-07-21 17:47:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const drivers= ['Milo','Otis','Garfield'];
+
+function destructivelyAppendDriver(name){
+  drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  drivers.append('Broom');
+}
